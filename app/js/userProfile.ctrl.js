1522523db3ba7c35548df6fad95257da059c56bc
asif-web-engineer <Asif-SoftwareEngineer@users.noreply.github.com>
2016-03-24 16:03:08 +1100

    $http service written

--- app/js/userProfile.ctrl.js
+++ app/js/userProfile.ctrl.js
@@ -1,46 +1,43 @@
 
 var myApp = angular.module('app_test_hudson', []);
 
-myApp.controller("UserProfileController", ['$scope','updateUseronServer', function($scope, updateUseronServer){
+myApp.controller("UserProfileController", ['$scope','$http', function($scope, $http){
     
     $scope.user = {};
     
     $scope.update = function(user){
-        if (user.firstName == undefined || !user.firstName.length) {
-            console.log("user first name invalid");
-            $scope.isFirstNameInValid = true;
-        }
-        else {
-            $scope.isFirstNameInValid = false;
-        }
         
-        if (user.lastName == undefined || !user.lastName.length) {
-            console.log("user last name invalid");
-            $scope.isLastNameInValid = true;
-        }
-        else {
-            $scope.isLastNameInValid = false;
-        }
+      $scope.isFirstNameInValid = false;
+      $scope.isLastNameInValid = false;
+      $scope.isValidForm = false;
+      
+
+        $http({
+            method: "POST",
+            data: JSON.stringify(user),
+            url: "http://localhost:65089/api/user",  // update this to the url where the api is running
+            contentType: "application/json"
+        }).then ( function success(response){
+            $scope.isValidForm = true;
+        }, function error(response){
+            if (response.data.Message == "FirstName invalid"){
+                $scope.isValidForm = false;
+                $scope.isFirstNameInValid = true;
+            }
+            if (response.data.Message == "LastName invalid"){
+                $scope.isValidForm = false;
+                $scope.isLastNameInValid = true;                
+            }
+        });
         
         if( $scope.isFirstNameInValid || $scope.isLastNameInValid ) {
             return;
         }
-        
-        console.log(user.firstName);
-        console.log(user.lastName);
-        console.log(user.country);
-        
-        updateUseronServer(user);
     }; 
   
-  $scope.isFirstNameInValid = false;
-  $scope.isLastNameInValid = false;
+
   
   $scope.pageTitle = "A simple online Form Using AngularJS and Restful WebApi";
   $scope.pageDescription = "A form with some basic server side validation";
-}]).
-factory('updateUseronServer', ['$window', function(win) {
-   return function(userData) {
-       win.alert("User with first Name" + userData.firstName + "has been submitted");
-       }
- }]);
+}]);
+
